59037ac94efcc0817dd3fcdd4778051503694fb3
HeyZeusV <valdesj419@gmail.com>
2024-01-23 19:52:56 -0800

    Added AlertDialog that is displayed when user wins, which will display game stats (when implemented) and allows user to start a new game. Doesn't allow dismissal as there isn't anything else a user can/should do.

--- app/src/main/java/com/heyzeusv/solitaire/Board.kt
+++ app/src/main/java/com/heyzeusv/solitaire/Board.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -56,13 +55,10 @@ fun SolitaireBoard(boardViewModel: BoardViewModel = viewModel()) {
     }
     val waste by remember { mutableStateOf(boardViewModel.waste.pile) }
 
-    val gameWon by boardViewModel.gameWon.collectAsState()
-
-    if (gameWon) {
-        boardViewModel.reset()
-    }
     Box(
-        modifier = Modifier.fillMaxSize().background(brush)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush)
     ) {
         Column(
             modifier = Modifier
--- app/src/main/java/com/heyzeusv/solitaire/MainActivity.kt
+++ app/src/main/java/com/heyzeusv/solitaire/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.heyzeusv.solitaire.ui.theme.SolitaireTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,6 +19,20 @@ class MainActivity : ComponentActivity() {
         boardVM.reset()
         setContent {
             SolitaireTheme(darkTheme = true) {
+                val gameWon by boardVM.gameWon.collectAsState()
+
+                if (gameWon) {
+                    AlertDialog(
+                        onDismissRequest = { },
+                        confirmButton = {
+                            TextButton(onClick = { boardVM.reset() }) {
+                                Text(text = "New Game")
+                            }
+                        },
+                        title = { Text(text = "You Won!") },
+                        text = { Text(text = "Congratulations! Here are your stats...") } // TODO: Add stats
+                    )
+                }
                 SolitaireApp(boardVM)
             }
 
