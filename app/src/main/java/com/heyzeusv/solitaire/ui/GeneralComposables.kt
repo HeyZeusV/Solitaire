e520ad79ed747a73bdca9aee8ff700fa0dea6b2c
HeyZeusV <valdesj419@gmail.com>
2024-03-31 14:25:26 -0700

    Refactored MenuOptionButton into BaseButton which will replace SolitaireButton next.

--- app/src/main/java/com/heyzeusv/solitaire/ui/GeneralComposables.kt
+++ app/src/main/java/com/heyzeusv/solitaire/ui/GeneralComposables.kt
@@ -1,19 +1,25 @@
 package com.heyzeusv.solitaire.ui
 
+import androidx.compose.animation.core.Transition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,7 +31,60 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.heyzeusv.solitaire.R
 import com.heyzeusv.solitaire.util.MenuState
-import com.heyzeusv.solitaire.util.SolitairePreview
+import com.heyzeusv.solitaire.util.PreviewUtil
+import com.heyzeusv.solitaire.util.getContentColor
+
+/**
+ *  Custom Button Composable that uses [Box] rather than standard [OutlinedButton] to allow for
+ *  easier animations using [Transition]. Displays given [icon] with content description of
+ *  [iconContentDesc] and given [buttonText]. This custom Button is very bare bones, so it requires
+ *  [modifier] to provide additional styling. [enabled] is used to determine which colors from
+ *  [buttonColors] should be used and if [onClick] is ran when Button is clicked.
+ */
+@Composable
+fun BaseButton(
+    icon: Painter,
+    iconContentDesc: String,
+    buttonText: String,
+    modifier: Modifier,
+    enabled: Boolean = true,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = Color.Transparent,
+        contentColor = Color.White
+    ),
+    onClick: () -> Unit
+) {
+    val contentColor = buttonColors.getContentColor(enabled)
+
+    Box(
+        modifier = modifier.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = rememberRipple(color = contentColor),
+            enabled = enabled,
+            onClick = onClick
+        ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = iconContentDesc,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+                tint = contentColor
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text(
+                text = buttonText,
+                color = contentColor,
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
 
 /**
  *  [onClick] is called when button is pressed. [icon] refers to the drawable to be display next to
@@ -95,22 +154,48 @@ fun MenuHeaderBar(
 
 @Preview
 @Composable
+fun BaseButtonPreview() {
+    PreviewUtil().apply {
+        Preview {
+            Row {
+                BaseButton(
+                    icon = painterResource(R.drawable.button_reset),
+                    iconContentDesc = "",
+                    buttonText = "Enabled",
+                    modifier = Modifier
+                ) { }
+                BaseButton(
+                    icon = painterResource(R.drawable.button_reset),
+                    iconContentDesc = "",
+                    buttonText = "Disabled",
+                    modifier = Modifier,
+                    enabled = false
+                ) { }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
 fun SolitaireButtonPreview() {
-    SolitairePreview {
-        Row {
-            SolitaireButton(
-                onClick = { },
-                icon = painterResource(R.drawable.button_reset),
-                iconContentDes = "",
-                buttonText = "Enabled"
-            )
-            SolitaireButton(
-                onClick = { },
-                icon = painterResource(R.drawable.button_reset),
-                iconContentDes = "",
-                buttonText = "Disabled",
-                enabled = false
-            )
+    PreviewUtil().apply {
+        Preview {
+            Row {
+                SolitaireButton(
+                    onClick = { },
+                    icon = painterResource(R.drawable.button_reset),
+                    iconContentDes = "",
+                    buttonText = "Enabled"
+                )
+                SolitaireButton(
+                    onClick = { },
+                    icon = painterResource(R.drawable.button_reset),
+                    iconContentDes = "",
+                    buttonText = "Disabled",
+                    enabled = false
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/heyzeusv/solitaire/ui/tools/Menu.kt
+++ app/src/main/java/com/heyzeusv/solitaire/ui/tools/Menu.kt
@@ -15,30 +15,16 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
@@ -47,16 +33,13 @@ import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.heyzeusv.solitaire.R
-import com.heyzeusv.solitaire.ui.SolitaireButton
+import com.heyzeusv.solitaire.ui.BaseButton
 import com.heyzeusv.solitaire.ui.game.GameViewModel
 import com.heyzeusv.solitaire.ui.scoreboard.ScoreboardViewModel
 import com.heyzeusv.solitaire.util.MenuState
-import com.heyzeusv.solitaire.util.PreviewDevices
-import com.heyzeusv.solitaire.util.SolitairePreview
 
 /**
  *  Composable that displays Menu options which transition into Menu screens.
@@ -269,74 +252,16 @@ fun MenuOptionTransition(
         ) { state ->
             when (state) {
                 option -> content()
-                MenuState.Buttons, MenuState.ButtonsFromScreen -> MenuOptionButton(
-                    option = option,
-                    onClick = { updateMenuState(option) }
-                )
+                MenuState.Buttons, MenuState.ButtonsFromScreen -> BaseButton(
+                    icon = painterResource(option.iconId),
+                    iconContentDesc = stringResource(option.iconDescId),
+                    buttonText = stringResource(option.nameId),
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth(0.3f)
+                ) { updateMenuState(option) }
                 else -> {}
             }
-        }
-    }
-}
-
-/**
- *  Copy of [SolitaireButton] Composable built from Box rather than Button in order for
- *  [AnimatedContent] to work correctly. [option] contains the text/icon data to be displayed.
- *  [onClick] is ran when pressed.
- */
-@Composable
-fun MenuOptionButton(
-    option: MenuState,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = modifier
-            .height(40.dp)
-            .fillMaxWidth(0.3f)
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(color = Color.White),
-                onClick = onClick
-            ),
-        contentAlignment = Alignment.Center,
-    ) {
-        Row(
-            modifier = Modifier.padding(horizontal = 24.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(option.iconId),
-                contentDescription = stringResource(option.iconDescId),
-                modifier = Modifier.size(ButtonDefaults.IconSize),
-                tint = Color.White
-            )
-            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-            Text(
-                text = stringResource(option.nameId),
-                color = Color.White,
-                style = MaterialTheme.typography.labelLarge
-            )
-        }
-    }
-}
-
-@PreviewDevices
-@Preview
-@Composable
-fun MenuOptionButtonPreview() {
-    SolitairePreview {
-        Box(modifier = Modifier
-            .padding(12.dp)
-            .fillMaxWidth()) {
-            MenuOptionButton(
-                option = MenuState.Stats,
-                onClick = { },
-                modifier = Modifier
-                    .fillMaxWidth(0.3f)
-                    .background(Color.Gray)
-            )
         }
     }
 }
