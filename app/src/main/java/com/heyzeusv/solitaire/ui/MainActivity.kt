b5c660dfc59cd60e28340b2bee9b195eb0612a8b
HeyZeusV <valdesj419@gmail.com>
2024-04-06 04:10:28 -0700

    Added SplashScreen which currently just displays app icon for a period between 2.5-3.5 seconds before fading away to show GameScreen.

--- app/src/main/java/com/heyzeusv/solitaire/ui/MainActivity.kt
+++ app/src/main/java/com/heyzeusv/solitaire/ui/MainActivity.kt
@@ -3,10 +3,16 @@ package com.heyzeusv.solitaire.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,11 +22,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.navigation.NavHostController
@@ -79,12 +89,14 @@ fun SolitaireApp(
         onPauseOrDispose { sbVM.pauseTimer() }
     }
 
-    NavHost(navController = navController, startDestination = NavScreens.Splash.route) {
+    NavHost(
+        navController = navController,
+        startDestination = NavScreens.Splash.route,
+        enterTransition = { fadeIn(animationSpec = tween(1500)) },
+        exitTransition = { fadeOut(animationSpec = tween(1500)) }
+    ) {
         composable(route = NavScreens.Splash.route) {
-            LaunchedEffect(key1 = Unit) {
-                delay(3000)
-                navController.navigate(NavScreens.Game.route)
-            }
+            SplashScreen(navController = navController)
         }
         composable(route = NavScreens.Game.route) {
             GameScreen(
@@ -147,4 +159,37 @@ fun GameScreen(
     }
     CloseGameAlertDialog(sbVM = sbVM, menuVM = menuVM, finishApp = finishApp)
     GameWonAlertDialog(sbVM = sbVM, gameVM = gameVM, menuVM = menuVM)
+}
+
+/**
+ *  First screen that user sees in order to give Settings to load correct game. Uses
+ *  [navController] to navigate to [GameScreen] after short duration.
+ */
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    LaunchedEffect(key1 = Unit) {
+        val delay = (2500L..3500L).random()
+        delay(delay)
+        navController.navigate(NavScreens.Game.route)
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        Box(modifier = Modifier.align(Alignment.Center)) {
+            Image(
+                painter = painterResource(R.mipmap.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier.shadow(
+                    elevation = dimensionResource(R.dimen.aImageElevation),
+                    shape = RoundedCornerShape(dimensionResource(R.dimen.aImageRoundSize))
+                )
+            )
+            Image(
+                painter = painterResource(R.mipmap.ic_launcher_foreground),
+                contentDescription = stringResource(R.string.app_name)
+            )
+        }
+    }
 }
