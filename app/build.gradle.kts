5143e7b3b959b956d3a0f0469a900cbc488e1f2b
HeyZeusV <valdesj419@gmail.com>
2024-05-08 09:46:34 -0700

    Prep for v4.2.0 release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.heyzeusv.solitaire"
         minSdk = 24
         targetSdk = 34
-        versionCode = 18
-        versionName = "4.1.0"
+        versionCode = 19
+        versionName = "4.2.0"
 
         testInstrumentationRunner = "com.heyzeusv.solitaire.util.CustomTestRunner"
         vectorDrawables {
